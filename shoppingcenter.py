1c2e13e1ac6b2fd333e667072c2f15e250faf2bf
eu-snehagupta <eu.snehagupta@gmail.com>
2020-03-20 21:09:41 +0100

    1.adding buisness case in README.md 2.adding feature for accepting all character case type as input

--- shoppingcenter.py
+++ shoppingcenter.py
@@ -1,5 +1,5 @@
 import json
-#FUNCTION DEFINATIONS
+# this function displays the welcome screen to the customer
 def welcomeScreen():
     print("Hi. Welcome to our store!")
     print("Here are the stock details--")
@@ -10,28 +10,28 @@ Stock = '''
 {
     "Items":[
             {
-                "item_type":"Shirt",
+                "item_type":"shirt",
                 "code":"001",
                 "count": "30",
                 "cost": "30",
                 "currency":"SEK"
             },
             {
-                "item_type":"Pant",
+                "item_type":"pant",
                 "code":"002",
                 "count": "40",
                 "cost": "40",
                 "currency":"SEK"
             },
             {
-                "item_type":"Cap",
+                "item_type":"cap",
                 "code":"003",
                 "count": "20",
                 "cost": "20",
                 "currency":"SEK"
             },
             {   
-                "item_type":"Socks",
+                "item_type":"socks",
                 "code":"004",
                 "count": "10",
                 "cost": "10",
@@ -41,29 +41,30 @@ Stock = '''
 }  '''
 
 stock = json.loads(Stock)
-
+# this funtion fetch the cost of the item baised on the input of multiple_order function
 def shoplist(x):
     for stock_details in stock['Items']:
-        if x == "Shirt":
-            if(stock_details['item_type']=="Shirt"):
+        if x == "shirt":
+            if(stock_details['item_type']=="shirt"):
                 return stock_details['cost']
-        elif x == "Pant":
-            if(stock_details['item_type']=="Pant"):
+        elif x == "pant":
+            if(stock_details['item_type']=="pant"):
                 return stock_details['cost']
-        elif x == "Cap":
-            if(stock_details['item_type']=="Cap"):
+        elif x == "cap":
+            if(stock_details['item_type']=="cap"):
                 return stock_details['cost']
-        elif x == "Socks":
-            if(stock_details['item_type']=="Socks"):
+        elif x == "socks":
+            if(stock_details['item_type']=="socks"):
                 return stock_details['cost']
 
+# this function fetch the order details of the customer
 def multiple_order(payment_amount):
-    more_order = input("Do you want to place an order?[Yes/No]")
+    more_order = input("Do you want to place an order?[yes/no]: ").lower()
     
-    if more_order == "Yes":
-        x = input("Enter your order details:")
+    if more_order == "yes":
+        x = input("Enter your order details:").lower()
         return multiple_order(payment_amount + int(shoplist(x)))
-    elif more_order == "No":
+    elif more_order == "no":
         return payment_amount 
 
 welcomeScreen()
