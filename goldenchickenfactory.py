9172719fcea21870c8087f8015696a07296be571
eu-snehagupta <eu.snehagupta@gmail.com>
2020-03-27 02:10:57 +0100

    code sanitization

--- goldenchickenfactory.py
+++ goldenchickenfactory.py
@@ -1,24 +1,37 @@
-def calculate_chickens(no_last_day,d_rate,b_rate,current_day):
-    total_death = (no_last_day*d_rate)/100
-    total_birth = (no_last_day*b_rate)/100
+def calculate_percentresult(principle,rate):
+    return (principle*rate)/100
     
+def calculate_chickens(no_last_day,d_rate,b_rate,current_day):
+    total_death = calculate_percentresult(no_last_day,d_rate)
+    total_birth = calculate_percentresult(no_last_day,b_rate)
+
     if current_day=="monday" or current_day=="wednesday":
-        further_death = (no_last_day*5)/100
-        
+        further_death = calculate_percentresult(no_last_day,5)
     elif current_day=="tuesday" or current_day=="thrusday" or current_day=="friday" or current_day=="saturday" or current_day=="sunday":
         further_death = 0
     else:
         print("Invalid input")
         exit()
 
-    count_chicken = str(no_last_day - total_death + total_birth + further_death)
+    count_chicken = str(no_last_day - total_death + total_birth - further_death)
     return count_chicken
 
+def welcomescreen():
+    print("********************************************")
+    print("******Hi. Welcome to Golden Chicken!!*******")
+    print("********************************************")
 
+def inputscreen():
+    no_last_day = input("Enter number of chickens last day: ")
+    d_rate = input("Enter rate of death of chickens: ")
+    b_rate = input("Enter rate of birth of chickens: ")
+    current_day = input("Enter todays day: ").lower()
+    return no_last_day, d_rate, b_rate, current_day
 
-no_last_day = input("Enter number of chickens last day: ")
-d_rate = input("Enter rate of death of chickens: ")
-b_rate = input("Enter rate of birth of chickens: ")
-current_day = input("Enter todays day: ").lower()
-no_of_chicken_today = calculate_chickens(int(no_last_day),int(d_rate),int(b_rate),current_day)
-print("Number on chicken on a current date: " +no_of_chicken_today)
+def outputscreen(no_last_day, d_rate, b_rate, current_day):
+    print("Number on chicken on a current date: " + calculate_chickens(int(no_last_day), int(d_rate), int(b_rate), current_day))
+    print("ThankYou")
+
+welcomescreen()
+no_last_day, d_rate, b_rate, current_day = inputscreen()
+outputscreen(no_last_day, d_rate, b_rate, current_day)
