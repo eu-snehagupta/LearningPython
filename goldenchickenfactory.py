e2651d77e7016667bc0c88b0368b8f3d1e13476e
eu-snehagupta <eu.snehagupta@gmail.com>
2020-03-27 02:22:46 +0100

    adding comments

--- goldenchickenfactory.py
+++ goldenchickenfactory.py
@@ -1,6 +1,13 @@
+#author:sneha
+#date:27_03_2020
+
+#defines the calculation for percentage
+#input:principle, rate
 def calculate_percentresult(principle,rate):
     return (principle*rate)/100
-    
+
+#defines the calculation for no of chicken on current day
+#input:no_last_day,d_rate,b_rate,current_day
 def calculate_chickens(no_last_day,d_rate,b_rate,current_day):
     total_death = calculate_percentresult(no_last_day,d_rate)
     total_birth = calculate_percentresult(no_last_day,b_rate)
@@ -16,11 +23,14 @@ def calculate_chickens(no_last_day,d_rate,b_rate,current_day):
     count_chicken = str(no_last_day - total_death + total_birth - further_death)
     return count_chicken
 
+#prints welcomescreen
 def welcomescreen():
     print("********************************************")
     print("******Hi. Welcome to Golden Chicken!!*******")
     print("********************************************")
 
+#take input informations
+#output:no_last_day, d_rate, b_rate, current_day
 def inputscreen():
     no_last_day = input("Enter number of chickens last day: ")
     d_rate = input("Enter rate of death of chickens: ")
@@ -28,10 +38,12 @@ def inputscreen():
     current_day = input("Enter todays day: ").lower()
     return no_last_day, d_rate, b_rate, current_day
 
+#prints the output results
 def outputscreen(no_last_day, d_rate, b_rate, current_day):
     print("Number on chicken on a current date: " + calculate_chickens(int(no_last_day), int(d_rate), int(b_rate), current_day))
     print("ThankYou")
 
+#main
 welcomescreen()
 no_last_day, d_rate, b_rate, current_day = inputscreen()
 outputscreen(no_last_day, d_rate, b_rate, current_day)
