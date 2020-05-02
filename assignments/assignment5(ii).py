1ced6b6804d780f02ed08d7793c568926dda0b3d
eu-snehagupta <eu.snehagupta@gmail.com>
2020-05-02 18:57:49 +0200

    adding in new assignment.py file

---
+++ assignments/assignment5(ii).py
@@ -0,0 +1,22 @@
+def quick_sort(sequence):
+    length = len(sequence)
+    if length <= 1:
+        return sequence
+    else:
+        pivot = sequence.pop()
+    grater_than = []
+    less_than = []
+    for items in sequence:
+        if items > pivot:
+            grater_than.append(items)
+        else:
+            less_than.append(items)
+    return quick_sort(less_than) + [pivot] + quick_sort(grater_than)
+
+if __name__ == "__main__":
+    arr = [45, 67, 23, 43, 25, 45, 54, 89, 67, 56]
+    sorted_arr = quick_sort(arr)
+    print("Unsorted sequence: ", arr)
+    print("Sorted sequence: ", sorted_arr)
+    print("Fourth highest element: ", sorted_arr[len(arr)-3])
+    print("Second lowest element: ", sorted_arr[1])
