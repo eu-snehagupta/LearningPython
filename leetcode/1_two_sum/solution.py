140e0766d9f93fd76942c6a9068db4684dd7c128
eu-snehagupta <eu.snehagupta@gmail.com>
2020-05-23 17:59:25 +0200

    check in codes of leetcode after submission

--- leetcode/1_two_sum/solution.py
+++ leetcode/1_two_sum/solution.py
@@ -1,14 +1,28 @@
 # Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-def solution(input,target):
-    lenght = len(input)
-    for i in range(0, lenght):
-        if input[i] + input[i + 1] == target:
-            return i, i+1
-        else:
-            i = i+1
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# Example:
+# Given nums = [2, 7, 11, 15], target = 9,
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+class Solution:
+    def twoSum(self, nums, target):
+        lenght = len(nums)
+        for i in range(0, lenght):
+            for j in range(i+1, lenght):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                else:
+                    j = j+1
+            i = i + 1
 
 if __name__ == "__main__":
-    input = [0, 1, 2, 7, 11, 15]
-    target = 9
-    index1, index2 = solution(input, target)
-    print("[{},{}]".format(index1, index2))
+    nums= list()
+    lenght = int(input("Enter the length of the list: "))
+    for i in range (0,lenght):
+        numberlist = int(input("Enter the number list: "))
+        nums.append(numberlist)
+    target = int(input("Enter the traget number: "))
+    output = Solution ()
+    print(output.twoSum(nums,target))
--- leetcode/7_reverse_integer/solution.py
+++ leetcode/7_reverse_integer/solution.py
@@ -1,47 +1,40 @@
-# Given a 32-bit signed integer, reverse digits of an integer.
+# # Given a 32-bit signed integer, reverse digits of an integer.
 
-# Example 1:
-# Input: 123
-# Output: 321
+# # Example 1:
+# # Input: 123
+# # Output: 321
 
-# Example 2:
-# Input: -123
-# Output: -321
+# # Example 2:
+# # Input: -123
+# # Output: -321
 # 
 # # Example 3:
-# Input: 120
-# Output: 21
+# # Input: 120
+# # Output: 21
+# # Note:
+# # Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1]. 
+# # For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
 class Solution:
     def reverse(self, data: int) -> int:
-        inp = data
+        if data < 0:inp = -data
+        elif data > 0:inp = data
+        else:return 0
         rev = 0
         while(inp!=0):
             rem = int(inp%10)
             rev = rev*10 + rem
             inp = int(inp/10)
-        return rev
+        if ((-2**31) <= rev <= (2**31 - 1)) and (data < 0):
+            return -rev
+        elif ((-2**31) <= rev <= (2**31 - 1)) and (data > 0):
+            return rev
+        else:
+            return 0
 
 if __name__ == "__main__":
     output = Solution()
     data = int(input("Enter you digit: "))
-    if data  < 0:
-        print(-1*(output.reverse(-1*data)))
-    else:
-        print(output.reverse(data))
+    print(output.reverse(data))
 
-
-# class Solution:
-#     def returninput(self, data: int) -> int:
-#         if(data<0):
-#             return -1 * reverse(-1*data)
-#         else:
-#             return reverse(data)
-#     def reverse(self, data: int) -> int:
-#         inp = data
-#         rev = 0
-#         while(inp!=0):
-#             rem = int(inp%10)
-#             rev = rev*10 + rem
-#             inp = int(inp/10)
-#         return rev
+        
