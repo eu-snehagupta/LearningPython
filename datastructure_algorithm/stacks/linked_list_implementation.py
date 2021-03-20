59ed6f6244630c8d0e3a9a6568d92ef072bdc2b7
Sneha Gupta <eu.snehagupta@gmail.com>
2021-03-20 13:02:26 +0100

    Add code for stack implementation using singly linked list

---
+++ datastructure_algorithm/stacks/linked_list_implementation.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        if self.head is None:
+            return True
+        else:
+            return False
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def pop(self):
+        if self.is_empty():
+            print("Empty stack.")
+            return
+        temp = self.head
+        if self.head.next is not None:
+            self.head = self.head.next
+        else:
+            self.head = None
+        temp.next = None
+        return temp.data
+
+    def top(self):
+        if self.is_empty():
+            print("Empty stack.")
+            return
+        return self.head.data
+
+    def print(self):
+        if self.is_empty():
+            print("Empty stack.")
+            return
+        current = self.head
+        while current is not None:
+            print(current.data, end= " ")
+            current = current.next
+
+
+if __name__ == '__main__':
+    stack = Stack()
+    print(stack.is_empty())
+    stack.pop()
+    stack.push(7)
+    stack.push(2)
+    stack.print()
+    print("\n", stack.is_empty())
+    print(stack.pop())
+    stack.push(4)
+    stack.print()
+    print("\n", stack.top())
+    stack.print()
+
+
