d28d0d61606ba794774cb58e4cb695d7df58b31d
Sneha Gupta <eu.snehagupta@gmail.com>
2021-03-20 12:21:32 +0100

    Add changes in stack implementation using Array

--- datastructure_algorithm/stacks/array_implementation.py
+++ datastructure_algorithm/stacks/array_implementation.py
@@ -24,14 +24,16 @@ def pop(stack):
 def top(stack):
     if is_empty(stack):
         return str(-maxsize - 1)
-    return stack[len(stack)-1]
+    return stack[-1]
 
 
 if __name__ == '__main__':
-    stack = create_stack()
-    push(stack, 3)
-    push(stack, 5)
-    push(stack, 8)
-    print(pop(stack))
-    print(stack)
+    s = create_stack()
+    push(s, 3)
+    push(s, 5)
+    push(s, 8)
+    print("Popped: ", pop(s))
+    print("Print stack: ", s)
+    print("Peek: ", top(s))
+
 
