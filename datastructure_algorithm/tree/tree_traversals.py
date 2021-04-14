9fb421e5512b393baa6f182745fbad517a0c94bb
Sneha Gupta <eu.snehagupta@gmail.com>
2021-04-14 11:48:06 +0200

    Add code for binary tree level order transveral

--- datastructure_algorithm/tree/tree_traversals.py
+++ datastructure_algorithm/tree/tree_traversals.py
@@ -1,43 +1,90 @@
 class Node:
     def __init__(self, val):
         self.val = val
-        self.left = None
-        self.right = None
+        self.left = self.right =  None
 
 
-def print_pre_order(root):
-    if root:
-        print(root.val)
-        print_pre_order(root.left)
-        print_pre_order(root.right)
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def enqueue(self, val):
+        return self.queue.append(val)
+
+    def dequeue(self):
+        return self.queue.pop(0)
 
 
-def print_in_order(root):
-    if root:
-        print_in_order(root.left)
-        print(root.val)
-        print_in_order(root.right)
+class BinaryTree:
+    def __init__(self):
+        self.root = None
 
+    def insert(self, root, val):
+        if root is None:
+            self.root = Node(val)
+            return self.root
 
-def print_post_order(root):
-    if root:
-        print_post_order(root.left)
-        print_post_order(root.right)
-        print(root.val)
+        if root.val >= val:
+            current = self.insert(root.left, val)
+            root.left = current
+        else:
+            current = self.insert(root.right, val)
+            root.right = current
+        return root
+
+    def print_pre_order(self, root):
+        if root:
+            print(root.val)
+            self.print_pre_order(root.left)
+            self.print_pre_order(root.right)
+
+    def print_in_order(self, root):
+        if root:
+            self.print_in_order(root.left)
+            print(root.val)
+            self.print_in_order(root.right)
+
+    def print_post_order(self, root):
+        if root:
+            self.print_post_order(root.left)
+            self.print_post_order(root.right)
+            print(root.val)
+
+    def print_level_order(self, root):
+        if root is None:
+            return
+        directory_queue = Queue()
+        directory_queue.enqueue(root)
+        while not(directory_queue.is_empty()):
+            current = directory_queue.dequeue()
+            print(current.val, end=" ")
+            if current.left:
+                directory_queue.enqueue(current.left)
+            if current.right:
+                directory_queue.enqueue(current.right)
 
 
 if __name__ == '__main__':
-    root = Node(1)
-    root.left = Node(2)
-    root.right = Node(3)
-    root.left.left = Node(4)
-    root.left.right = Node(5)
 
-    print("Preorder traversal: ")
-    print_pre_order(root)
+    data = [3, 5, 4, 7, 2, 1]
 
-    print("Inorder traversal: ")
-    print_in_order(root)
+    binary_tree = BinaryTree()
+    root = None
 
-    print("Postorder traversal: ")
-    print_post_order(root)
+    for i in range(len(data)):
+        root = binary_tree.insert(root, data[i])
+
+    # print("Preorder traversal: ")
+    # binary_tree.print_pre_order(root)
+    #
+    # print("Inorder traversal: ")
+    # binary_tree.print_in_order(root)
+    #
+    # print("Postorder traversal: ")
+    # binary_tree.print_post_order(root)
+
+    print("Levelorder traversal: ")
+    binary_tree.print_level_order(root)
