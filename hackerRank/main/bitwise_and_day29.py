1be5da31869fb1f2a8a966d254785d19acfc30cc
Sneha Gupta <eu.snehagupta@gmail.com>
2021-04-19 13:52:42 +0200

    Improve code for bitwise and related question

--- hackerRank/main/bitwise_and_day29.py
+++ hackerRank/main/bitwise_and_day29.py
@@ -1,10 +1,10 @@
 def bitwiseAnd(N, K):
-    max_bitwise = 0
-    for i in range(1, N):
-        for j in range(i+1, N+1):
-            if max_bitwise < (i & j) < K:
-                max_bitwise = i & j
-    return max_bitwise
+    for k in range(K - 1, 0, -1):
+        for i in range(1, N):
+            for j in range(i+1, N+1):
+                if (i & j) == k:
+                    return k
+    return 0
 
 
 if __name__ == '__main__':
