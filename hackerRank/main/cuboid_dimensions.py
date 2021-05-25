b924058ce99cc3e3d1dbc7865144baa8531df8ee
Sneha Gupta <eu.snehagupta@gmail.com>
2021-05-25 18:32:04 +0200

    Add code for question based on python comprehensions

--- hackerRank/main/cuboid_dimensions.py
+++ hackerRank/main/cuboid_dimensions.py
@@ -1,10 +1,2 @@
-from itertools import *
-
-
-def cuboid_dimensions(x_,y_,z_,n_):
-    print([[a,b,c] for a in range(x+1) for b in range(y+1) for c in range(z+1) if a+b+c !=n])
-
-
-if __name__ == "__main__":
-    x,y,z,n = (int(input()) for _ in range(4))
-    cuboid_dimensions(x,y,z,n)
+x,y,z,n = (int(input()) for _ in range(4))
+print([[a,b,c] for a in range(x+1) for b in range(y+1) for c in range(z+1) if a+b+c !=n])
